e7180e1e73ff48b6a8d1d117e63743f4160b8640
iago-costa <univercomput@gmail.com>
2023-01-16 22:44:44 -0300

    chore: Updates settings files

--- src/handle_test.py
+++ src/handle_test.py
@@ -1,22 +1,48 @@
 # teste unitario para o modulo hendle.py
 
 import unittest
+
 import src.handle as handle
 
-class TestHandle(unittest.TestCase):
 
+class TestHandle(unittest.TestCase):
     def setUp(self):
-        self.new_file = 'new_file.csv'
-        self.old_file = 'TabeladeAreasdoConhecimento.csv'
+        self.new_file = "new_file.csv"
+        self.old_file = "TabeladeAreasdoConhecimento.csv"
 
     def test_get_list(self):
-        self.assertEqual(handle.get_list('0 Lógica Matemática 1.01.01.03-9'), ['1.00.00.00-3', 'Ciências Exatas e da Terra'])
+        self.assertEqual(
+            handle.get_list("0 Lógica Matemática 1.01.01.03-9"),
+            ["1.00.00.00-3", "Ciências Exatas e da Terra"],
+        )
 
     def test_read_csv(self):
         self.assertEqual(handle.read_csv(self.old_file).shape, (1290, 1))
 
     def test_save_csv(self):
-        self.assertEqual(handle.save_csv(self.new_file, handle.read_csv(self.old_file)).shape, (1290, 1))
+        self.assertEqual(
+            handle.save_csv(self.new_file, handle.read_csv(self.old_file)).shape,
+            (1290, 1),
+        )
 
     def test_line_by_line(self):
-        self.assertEqual(handle.line_by_line(self.new_file, self.old_file).shape, (1291, 2))
+        self.assertEqual(
+            handle.line_by_line(self.new_file, self.old_file).shape, (1291, 2)
+        )
+
+
+# write for pytest functions
+# def test_get_list():
+#     assert handle.get_list('0 Lógica Matemática 1.01.01.03-9') == ['Lógica Matemática', '1.01.01.03-9']
+
+
+# def test_read_csv():
+#     assert handle.read_csv('TabeladeAreasdoConhecimento.csv').shape == (1290, 1)
+
+
+# def test_save_csv():
+#     assert handle.save_csv('new_file.csv', handle.read_csv('TabeladeAreasdoConhecimento.csv')).shape == (1290, 1)
+
+
+# def test_line_by_line():
+#     assert handle.line_by_line('new_file.csv', 'TabeladeAreasdoConhecimento.csv').shape == (1291, 2)
